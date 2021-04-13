c2b7af08d91de13e447e07a1471384ed5bcf3ae8
brhopcra <68735117+brhopcra@users.noreply.github.com>
2021-04-13 08:03:11 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -l
 cd /bin/staticsites/
+DEPLOYMENT_ACTION=upload
+DEPLOYMENT_TOKEN="$secret"
+DEPLOYMENT_PROVIDER=BitBucket
+REPOSITORY_URL="https://github.com/BrandonHopcraft/ReactSite/"
+BRANCH=master
+REPOSITORY_BASE="/temp/static"
+
 ./StaticSitesClient $INPUT_ACTION --verbose
