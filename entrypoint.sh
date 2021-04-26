155ce9054b7a4315ac10fbad17925e8b3e9ababb
brhopcra <68735117+brhopcra@users.noreply.github.com>
2021-04-26 13:21:50 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo repourl
 echo $REPOSITORY_URL
 
 apt-get update
-apt-get install jq
+apt-get install jq -y
 
 REPOINFO=$( jq .repository $GITHUB_EVENT_PATH )
 REPOSITORY_URL=$( jq .repository.html_url $GITHUB_EVENT_PATH )
