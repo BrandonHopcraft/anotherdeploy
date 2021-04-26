e4051377f9fbba7e09c35f6d9b130b66798ef735
brhopcra <68735117+brhopcra@users.noreply.github.com>
2021-04-26 13:07:25 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,12 @@ echo $BRANCH
 echo repourl
 echo $REPOSITORY_URL
 
+REPOINFO=$( jq .repository $GITHUB_EVENT_PATH )
+REPOSITORY_URL=$( jq .repository.html_url $GITHUB_EVENT_PATH )
+
+echo $REPOINFO
+echo $REPOSITORY_URL
+
+
 
 ./StaticSitesClient $INPUT_ACTION --verbose
