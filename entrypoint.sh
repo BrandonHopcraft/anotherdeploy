e52e09bee24ecae99fb2bad2add1ece6bdab35c9
brhopcra <68735117+brhopcra@users.noreply.github.com>
2021-05-19 13:24:17 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ DEPLOYMENT_PROVIDER=GitHub
 REPOSITORY_URL="https://github.com/BrandonHopcraft/ReactSite/"
 BRANCH=master
 REPOSITORY_BASE="/temp/static"
+ENVIRONMENT_NAME="12345678901234567"
 
 ./StaticSitesClient $INPUT_ACTION --verbose
