f313175c201a976a052f7a97f4c33402c675e897
brhopcra <68735117+brhopcra@users.noreply.github.com>
2021-05-25 16:39:02 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,14 @@
 cd /bin/staticsites/
 export DEPLOYMENT_ACTION=upload
 export DEPLOYMENT_PROVIDER=GitHub
-export REPOSITORY_URL="https://github.com/BrandonHopcraft/ReactSite/"
+export REPOSITORY_URL="https://github.com/BrandonHopcraft/ReactSite"
 export BRANCH="master"
 REPOSITORY_BASE="/temp/static"
-export ENVIRONMENT_NAME="1-2-3"
+export ENVIRONMENT_NAME="123"
 export APP_LOCATION="app" # App source code path
 export OUTPUT_LOCATION="build" # Built app content directory - optional
 export APP_BUILD_COMMAND="npm run prodbuild"
 export BUILD_TIMEOUT_IN_MINUTES="5"
+export GITHUB_EVENT_PATH=""
 
 ./StaticSitesClient --verbose
